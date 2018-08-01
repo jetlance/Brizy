f73eefe82b992d2119db3f5c792ff2988a7bf042
Alex Zaharia <alecszaharia@gmail.com>
2018-08-01 16:48:38 +0300

    Rewrite rule form and list using hyperapp

--- public/views/templates/brizy-blank-template.php
+++ public/views/templates/brizy-blank-template.php
@@ -11,7 +11,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php while (have_posts()) : the_post() ?>
